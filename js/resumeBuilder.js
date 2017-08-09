89b58e7772f405322755c5b0478966fcdb35ca45
cepeda <carlos@cepeda.me>
2017-08-09 21:20:22 +0200

    Issues fixed

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,38 +15,21 @@ var bio = {
 
 bio.display = function() {
   var header = $('#header');
-  var topContacts = $('#topContacts');
-  var footerContacts = $('#footerContacts');
+  var contacts = $('#topContacts, #footerContacts');
 
   var formattedName = HTMLheaderName.replace("%data%", bio.name);
   var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
   var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
   var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
   var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
   var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
   var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-
   var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
   var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
-  header.prepend(formattedRole);
-  header.prepend(formattedName);
-
-  topContacts.append(formattedMobile);
-  topContacts.append(formattedEmail);
-  topContacts.append(formattedGithub);
-  topContacts.append(formattedTwitter);
-  topContacts.append(formattedLocation);
-  footerContacts.append(formattedMobile);
-  footerContacts.append(formattedEmail);
-  footerContacts.append(formattedGithub);
-  footerContacts.append(formattedTwitter);
-  footerContacts.append(formattedLocation);
-
-  header.append(formattedBioPic);
-  header.append(formattedWelcomeMsg);
-  header.append(HTMLskillsStart);
+  header.prepend(formattedName, formattedRole);
+  contacts.append(formattedMobile, formattedEmail, formattedGithub, formattedTwitter, formattedLocation);
+  header.append(formattedBioPic, formattedWelcomeMsg, HTMLskillsStart);
 
   bio.skills.forEach(function(skill) {
     var formattedSkill = HTMLskills.replace("%data%", skill);
@@ -60,14 +43,14 @@ var education = {
       "name": "Hogwarts School of Witchcraft and Wizardry",
       "location": "Highlands, Scotland",
       "degree": "Wizard" ,
-      "major": "Auror",
+      "major": ["Auror"],
       "dates": "2002 - 2007",
       "url": "http://harrypotter.wikia.com/wiki/Hogwarts_School_of_Witchcraft_and_Wizardry"
     }, {
       "name": "Beauxbatons Academy of Magic",
       "location": "Pyrenees, France",
       "degree": "Advance Wizard",
-      "major": "Advance Auror",
+      "major": ["Advance Auror"],
       "dates": "2007 - 2009",
       "url": "http://harrypotter.wikia.com/wiki/Beauxbatons_Academy_of_Magic"
     }
@@ -88,30 +71,21 @@ education.display = function() {
   var entry = $('.education-entry');
 
   education.schools.forEach(function(school) {
-    var formattedName =
-      HTMLschoolName.replace("%data%", school.name).replace("#", school.url) +
-      HTMLschoolDegree.replace("%data%", school.degree);
+    var formattedName = HTMLschoolName.replace("%data%", school.name).replace("#", school.url) + HTMLschoolDegree.replace("%data%", school.degree);
     var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
     var formattedMajor = HTMLschoolMajor.replace("%data%", school.major);
     var formattedDate = HTMLschoolDates.replace("%data%", school.dates);
 
-    entry.append(formattedName);
-    entry.append(formattedLocation);
-    entry.append(formattedDate);
-    entry.append(formattedMajor);
+    entry.append(formattedName, formattedLocation, formattedDate, formattedMajor);
   });
 
   entry.append(HTMLonlineClasses);
   education.onlineCourses.forEach(function(course) {
-    var formattedName =
-      HTMLonlineTitle.replace("%data%", course.title).replace("#", course.url) +
-      HTMLonlineSchool.replace("%data%", course.school);
+    var formattedName = HTMLonlineTitle.replace("%data%", course.title).replace("#", course.url) + HTMLonlineSchool.replace("%data%", course.school);
     var formattedDate = HTMLonlineDates.replace("%data%", course.dates);
     var formattedURL = HTMLonlineURL.replace("%data%", course.url).replace("#", course.url);
 
-    entry.append(formattedName);
-    entry.append(formattedDate);
-    entry.append(formattedURL);
+    entry.append(formattedName, formattedDate, formattedURL);
   });
 
 };
@@ -140,24 +114,19 @@ work.display = function() {
   var entry = $('.work-entry');
 
   work.jobs.forEach(function(job) {
-    var formattedJob =
-      HTMLworkEmployer.replace("%data%", job.employer) +
-      HTMLworkTitle.replace("%data%", job.title);
+    var formattedJob = HTMLworkEmployer.replace("%data%", job.employer) + HTMLworkTitle.replace("%data%", job.title);
     var formattedLocation = HTMLworkLocation.replace("%data%", job.location);
     var formattedDate = HTMLworkDates.replace("%data%", job.dates);
     var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
 
-    entry.append(formattedJob);
-    entry.append(formattedLocation);
-    entry.append(formattedDate);
-    entry.append(formattedDescription);
+    entry.append(formattedJob, formattedLocation, formattedDate, formattedDescription);
   });
 };
 
 var projects = [
   {
     "title": "The Corporeal Patronus",
-    "dates": 2007,
+    "dates": "2007",
     "description": "A corporeal Patronus is one that is fully-formed, taking the shape of bright-white, translucent animal. The specific animal forms that corporeal Patronuses take vary from person to person and reflect each individual's personality.",
     "images": [
       "images/pat1.png",
@@ -177,8 +146,7 @@ projects.display = function() {
       HTMLprojectDates.replace("%data%", project.dates);
     var formattedDescription = HTMLprojectDescription.replace("%data%", project.description);
 
-    entry.append(formattedProject);
-    entry.append(formattedDescription);
+    entry.append(formattedProject, formattedDescription);
 
     project.images.forEach(function(image) {
       var formattedImage = HTMLprojectImage.replace("%data%", image);
